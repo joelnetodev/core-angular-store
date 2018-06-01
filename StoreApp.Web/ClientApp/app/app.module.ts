8d44b3adf87404ecaf768594da699dac963d1764
Joel Neto <joedeathscythe@gmail.com>
2018-06-01 14:12:21 -0300

    final organization

--- StoreApp.Web/ClientApp/app/app.module.ts
+++ StoreApp.Web/ClientApp/app/app.module.ts
@@ -17,15 +17,16 @@ import { CoreServiceModule } from './modules/0-core/core.service.module';
 import { CoreSharedModule } from './modules/0-core/core.shared.module';
 import { CoreCompModule } from './modules/0-core/core.comp.module';
 
-import { AppComponent } from './components/0-core/app/app.component';
+import { AppComponent } from './components/app/app.component';
 import { HomeComponent } from './components/project/home/home.component';
 import { ProductsComponent } from './components/project/products/products.component';
 import { RowComponent } from './components/project/products/row/row.component';
 import { LoginComponent } from './components/project/login/login.component';
-import { NavTopComponent } from './components/0-core/navtop/navtop.component';
-import { PermissionComponent } from './components/0-core/permission/permission.component';
+import { NavTopComponent } from './components/project/navtop/navtop.component';
+import { PermissionComponent } from './components/project/permission/permission.component';
 
 import { AppInterceptor } from './app.interceptor';
+
 
 //NgModule to identify this class as a Module
 //The MainModule are global, so global services should be declared here to others Modules could use.
--- StoreApp.Web/ClientApp/app/app.routing.ts
+++ StoreApp.Web/ClientApp/app/app.routing.ts
@@ -3,7 +3,7 @@
 import { HomeComponent } from './components/project/home/home.component';
 import { LoginComponent } from './components/project/login/login.component';
 import { ProductsComponent } from './components/project/products/products.component';
-import { PermissionComponent } from './components/0-core/permission/permission.component';
+import { PermissionComponent } from './components/project/permission/permission.component';
 
 //RouterModule for override the path to components.
 //This allows to create routerLink on HTML to render components
