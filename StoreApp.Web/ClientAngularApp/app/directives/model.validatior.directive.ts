ad74d60f7c2e7f744837d3b222ea227836c43efb
Joel Neto <joelneto@m2ssoftware.com>
2018-07-05 14:50:26 -0300

    directive impromenets

--- StoreApp.Web/ClientAngularApp/app/directives/model.validatior.directive.ts
+++ StoreApp.Web/ClientAngularApp/app/directives/model.validatior.directive.ts
@@ -1,5 +1,7 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import { CoreModelValidatorService, ModelError } from '../services/0-core/core.model.validator.service';
+import { Renderer2 } from '@angular/core';
+import { Inject } from '@angular/core';
 
 declare var $: any;
 
@@ -20,14 +22,32 @@ export class ModelValidatorDirective implements OnInit {
     }
 
     ngAfterViewInit() {
+
+        let thisElement = this.elementRef.nativeElement;
+
         this.validServ.getModelErrors().subscribe(modelErrors => {
+
+            //get element p after the input model
+            let elementP = $(thisElement).next('p');
 
             for (let itemError of modelErrors) {
 
                 if (itemError.key == this.key) {
-                    this.elementRef.nativeElement.style.backgroundColor = '#F8D7DA';
-                    break;
+
+                    //if model has no P element, it means there is no error
+                    if (elementP.length == 0) {
+                        let errorEl = document.createElement('p');
+                        errorEl.innerHTML = itemError.value;
+                        errorEl.style.color = "#cc0000 ";
+                        thisElement.parentNode.insertBefore(errorEl, thisElement.nextSibling);
+                        return;
+                    }                    
                 }
+            }
+
+            //if no key found, it means that inout model has no error, so remove the element
+            if (elementP.length != 0) {
+                elementP.remove();
             }
         })
     }
