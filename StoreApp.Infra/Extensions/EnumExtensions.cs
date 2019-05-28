434277e2457bf44668d9a91034880b159a80ddcc
Joel Coutinho da Silva Neto <joel.neto.90@outlook.com>
2019-05-28 14:52:59 +0100

    enum extension

---
+++ StoreApp.Infra/Extensions/EnumExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+
+namespace SharedInfra.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string ToDescription(this Enum enumValue)
+        {
+            if (enumValue == null) return null;
+
+            FieldInfo fi = enumValue.GetType().GetField(enumValue.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes != null && attributes.Length > 0
+                  ? attributes[0].Description
+                  : enumValue.ToString();
+        }
+    }
+}
